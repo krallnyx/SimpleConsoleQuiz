8a685a485e5597361d5b2a3e8b2b24daea2006f6
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-01-07 13:41:23 +0000

    Using formating for Questions from OpenTrivia

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
-    question_bank.append(Question(q["text"], q["answer"]))
+    question_bank.append(Question(q["question"], q["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
